dc9eb63c2b92622f062e2ec5537a59268fc0d8c8
Arthur Albuquerque Dos reis <127634288+DevArthReis@users.noreply.github.com>
2024-10-16 16:12:20 -0300

    Cosntrutores

--- classes/classe2.py
+++ classes/classe2.py
@@ -1,16 +1,21 @@
 class Evento:
     def metodo_instancia(self):
-        return ("metodo de instancia chamado",self)
+        return ("Método de instância chamado", self)
+
     @classmethod
     def metodo_classe(cls):
-        return("Metodo de classe chamado ",cls)
-    @staticmethod#Por ser estatico este metodo é independente da classe evento
+        return ("Método de classe chamado", cls)
+
+    @staticmethod  # Por ser estático, este método é independente da classe Evento
     def metodo_estatico():
-        return"estatico chamado"
+        return "Estático chamado"
+
 ev = Evento()
-a = ev.metodo_instancia()# Evento.metodo_instancia()
-print(a)#('metodo de instancia chamado', <__main__.Evento object ) ele ira printar a função metodo _instancia e o self vai ser subistituido para mostrar que ele é um objeto da classe Evento
-b = Evento.metodo_classe()# Evento.metodo_clase(Evento)
-print(b)# Evento.metodo_clase(Evento)
-c = Evento.metodo_estatico()
+a = ev.metodo_instancia()  # Chama o método de instância
+print(a)  # ('Método de instância chamado', <__main__.Evento object>) ele irá imprimir a função metodo_instancia e o self será substituído para mostrar que ele é um objeto da classe Evento
+
+b = Evento.metodo_classe()  # Chama o método de classe
+print(b)  # ('Método de classe chamado', <class '__main__.Evento'>)
+
+c = Evento.metodo_estatico()  # Como ele é independente, ele apenas imprime o valor da função metodo_estatico sem mais nada
 print(c)
